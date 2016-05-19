046d3844ac39c371241ef153d97b40748abdc37b
Charlie Cook <turingbombe@gmail.com>
2016-05-19 11:50:23 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
- 
+  quotient = num1/num2
+  quotient
 end
 
 def assign_variable(value)
-  
+   name =  value
 end
 
-def argue
-  
+def argue(thing)
+  thing
 end
 
-def greeting
-
+def greeting(greeting, name)
+ 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  type
 end
+
+division(42, 7)
+assign_variable("Bob")
+argue("I'm right and you are wrong!")
+greeting("Hi there, ", "Bobby!")
